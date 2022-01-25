922caac11736f1d625e75f8122d3ba466b036a4a
nikkiqin120 <nikkiqin120@gmail.com>
2022-01-25 15:49:22 -0800

    .

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const express = require("express")
 const app = express()
+const router = express.Router()
+
 const cors = require("cors")
 app.use(cors({
-    origin:"*"//"http://localhost:8080"
+  origin: "*"//"http://localhost:8080"
 }))
 
-
-
 var tracker = require('delivery-tracker')
-var courier = tracker.courier("ups")
-var trace_number = "1Z9Y27V42063168543"
 
 
-courier.trace(trace_number, function (err, result) {
-//   console.log(JSON.stringify(result))
-    let path="/" + trace_number;
-  app.get(path, (req, res) => {
-    res.send(result)
+// app.get('/', (req,res)=>{
+//   console.log("router get")
+//   res.send(req.body)
+// })
+
+// app.get('/', (req, res) => {
+//   // res.sendStatus(200)
+//   // res.status(201).send("hi this ism e")
+//   res.json({message : "download"});
+//   // res.download("index.js")
+//   // res.send("req")
+// })
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.post('/', (req, res) => {
+
+  // console.log(req.body)
+  var courier = tracker.courier(req.body.cr)
+  courier.trace(req.body.id, (err, result) => {
+      if(err){
+        console.log(err)
+      }
+      else if (result){
+        res.status(201).send(result)
+      }
+  })
+
 })
-})
+
 
 app.listen(5000)
