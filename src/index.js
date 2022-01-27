82b9794157994d416034bfd8235e11f96cc3fb17
nikkiqin120 <nikkiqin120@gmail.com>
2022-01-27 14:54:11 -0800

    + post

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const express = require("express")
 const serverless = require("serverless-http");
-
+const tracker = require('delivery-tracker')
 const app = express()
 const router = express.Router()
 
 // const cors = require("cors")
 
 // app.use(cors({origin: "*"}))
-// app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 router.get('/', (req,res)=>{
   // console.log("app entry test")
   res.send("Hello Nikki")
+})
+
+router.post('/', (req, res) => {
+  // console.log(req.body)
+  var courier = tracker.courier(req.body.cr)
+  courier.trace(req.body.id, (err, result) => {
+      if(err){
+        console.log(err)
+      }
+      else if (result){
+        res.status(201).send(result)
+      }
+  })
 })
 
 app.use('/.netlify/functions/index', router);//require('./routes/api'));
