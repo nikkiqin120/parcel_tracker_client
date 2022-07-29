0438a546f8d01f539e7bc0c1c40738d422e80ef8
Ying Qin <yqin@NOVAX.local>
2022-07-29 22:33:36 +0100

    add database.js

--- src/index.js
+++ src/index.js
@@ -2,78 +2,14 @@ const express = require("express")
 const serverless = require("serverless-http");
 const tracker = require('../delivery-tracker_mod')
 const app = express()
-const router = express.Router()
-const mongodb = require('mongodb');
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+// const router = express.Router()
+// const cors = require("cors")
 
-const cors = require("cors")
-
-app.use(cors({ origin: "*" }))
-router.use(cors({ origin: '*' }))
+// app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const pwd = encodeURIComponent("Novax2022@")
-const uri = "mongodb+srv://nikkiqin120:" + pwd + "@ndata.zhcza.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(res => {
-    // console.log("connect to db0")
-  })
-  .catch(err => console.log(err))
-
-router.get('/', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch(err => console.log(err))
-})
-
-router.get('/find', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch(err => console.log(err))
-})
-
-router.get('/deleteall', async (req, res) => {
-  Blog.deleteMany()
-    .then(result => console.log("delete all: " + result))
-    .catch(err => console.log(err))
-})
-
-router.post('/add', (req, res) => {
-  const item = req.body.data
-  const blog = new Blog(item)
-
-  blog.save()
-    .then(result => res.send(result))
-    .catch(err => console.log(err))
-})
-
-router.post('/delete', async (req, res) => {
-  Blog.deleteOne({ tracknumber: req.body.data.tracknumber })
-    .then(result => console.log("delete CR: " + JSON.stringify(result)))
-    .catch(err => console.log(err))
-  // }
-})
-
-
-router.post('/', (req, res) => {
-  // console.log(req.body)
-  var courier = tracker.courier(req.body.cr)
-  courier.trace(req.body.id, (err, result) => {
-    if (err) {
-      console.log(err)
-    }
-    else if (result) {
-      res.status(201).send(result)
-    }
-  })
-})
+const dbRouter = require('./routes/database')
 
 ////***************For debug************/////
 
@@ -91,7 +27,7 @@ router.post('/', (req, res) => {
 //   }
 // })
 /////////////////
-app.use('/.netlify/functions/index', router); //require('../routes/api'));
+app.use('/.netlify/functions/index', dbRouter); //require('../routes/api'));
 ////*****************Chose one option from below************************/
 // 1. serverless in netlify
 module.exports = app;
